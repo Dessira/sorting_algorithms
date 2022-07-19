734b56f117b9a5bd5d462f83ecf7ed401921057a
emmanuel <mcdecoil@gmail.com>
2022-07-19 22:20:31 +0000

    switched to while loops

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,13 +15,17 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	i = 0;
+
+	while (i < size - 1)
 	{
 		min = i;
-		for (j = i + 1; j < size; j++)
+		j = 1 + 1;
+		while (j < size)
 		{
 			if (array[j] < array[min])
 				min = j;
+			j++;
 		}
 		if (i != min)
 		{
@@ -30,5 +34,6 @@ void selection_sort(int *array, size_t size)
 			array[min] = swap;
 			print_array(array, size);
 		}
+		i++;
 	}
 }
