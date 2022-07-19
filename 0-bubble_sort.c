4d0ce8ea9fe4b27a013295d8b49bb92798e40c42
emmanuel <mcdecoil@gmail.com>
2022-07-19 20:00:31 +0000

    code cleanup

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,30 +10,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int limit, holder;
-	size_t i, x;
+	unsigned int i, j;
+	int tmp;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; ++i)
+	for (i = 0; i < size; i++)
 	{
-		limit = 0;
-
-		for (x = 0; x < size - i - 1; ++x)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[x] > array[x + 1])
+			if (array[j] > array[j + 1])
 			{
-				holder = array[x];
-				array[x] = array[x + 1];
-				array[x + 1] = holder;
-				limit = 1;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
 			}
-			print_array(array, size);
-		}
-		if (limit == 0)
-		{
-			break;
 		}
 	}
 }
